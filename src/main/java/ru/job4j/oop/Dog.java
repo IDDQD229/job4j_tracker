bed31f53d14ee38bcc2e0ad8b2e24f3176c74eeb
IDDQD <bigsobolev@yandex.ru>
2021-09-22 14:44:31 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharkik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
