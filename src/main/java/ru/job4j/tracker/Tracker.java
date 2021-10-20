9a23343df342afea2d3e9a938170fbd65ddacacc
IDDQD <bigsobolev@yandex.ru>
2021-10-20 10:36:30 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,32 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll(){
+        Item[] itemsWithoutNull = new Item[size];
+        int count = 0;
+        for (Item item : items) {
+            if (item != null) {
+                itemsWithoutNull[count] = item;
+                count++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, count);
+        return itemsWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] findNames = new Item[size];
+        int rsl = 0;
+        for (Item name : items) {
+            if (name != null && name.getName().equals(key)) {
+                findNames[rsl] = name;
+                rsl++;
+            }
+        }
+        findNames = Arrays.copyOf(findNames,rsl);
+        return findNames;
     }
 
     public Item findById(int id) {
