93bafb277821d2de7bc268f5f2046f3ff1718335
IDDQD <bigsobolev@yandex.ru>
2021-10-27 16:38:18 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,9 +10,14 @@ public class StartUI {
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select != 6) {
-                System.out.println("Пользователь выбрал: " + select);
-            } else {
+           if(select == 0){
+               System.out.println("=== Create a new Item ===");
+               System.out.println("Enter name: ");
+               String name = scanner.nextLine();
+               Item item = new Item(name);
+               tracker.add(item);
+               System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
                 run = false;
             }
         }
@@ -27,7 +32,6 @@ public class StartUI {
         System.out.println("Menu:");
         for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
-
         }
     }
 
