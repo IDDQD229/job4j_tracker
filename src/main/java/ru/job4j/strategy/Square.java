3795cf27ac165310769aeff06ac0c909b5b225c6
IDDQD <bigsobolev@yandex.ru>
2021-11-05 00:10:42 +0300

    7. Шаблон проектирования - Стратегия [#786 #224457]

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -4,14 +4,10 @@ public class Square implements Shape {
     @Override
     public String draw() {
         String ln = System.lineSeparator();
-        return    " _______" + ln
+        return " _______" + ln
                 + "|       |" + ln
                 + "|       |" + ln
                 + "|_______|" + ln;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Square().draw());
     }
 }
 
