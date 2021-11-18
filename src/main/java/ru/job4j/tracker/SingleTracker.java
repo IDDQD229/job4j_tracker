14863087e3e41c519e55e3fef73abee3f003cbc8
IDDQD <bigsobolev@yandex.ru>
2021-11-18 18:06:06 +0300

    1. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -3,9 +3,17 @@ package ru.job4j.tracker;
 import java.util.Arrays;
 
 public class SingleTracker {
+    private static SingleTracker instance;
     private Tracker tracker = new Tracker();
 
     private SingleTracker() {
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
     }
 
     public Item add(Item item) {
